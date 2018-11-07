9298ac2d23c964aac2b30a662f8ba1c6948ab3da
Luxray93 <john117654@gmail.com>
2018-11-07 14:04:49 +0100

    4 metoder reee

--- src/enums_reeeeeeeeeeeeeeee/BigboieFysikgrej.java
+++ src/enums_reeeeeeeeeeeeeeee/BigboieFysikgrej.java
@@ -3,19 +3,22 @@ package enums_reeeeeeeeeeeeeeee;
 public class BigboieFysikgrej {
 	public static void main(String[] args) {
 	}
-		public static double fahrenheittocelsius(double fahrenheit) {
-			
-			double fahrenheit2 = (fahrenheit-32)/1.8;
-			return fahrenheit2;
-			
-		}
-		public static double kelvintocelsius(double kelvin) {
-			double celsius = kelvin-273.15;
-			return celsius;
-		}
-		public static double fluidpressure
-		
-		
-		// big 
+
+	public static double fahrenheittocelsius(double fahrenheit) {
+
+		double fahrenheit2 = (fahrenheit - 32) / 1.8;
+		return fahrenheit2;
+
 	}
 
+	public static double kelvintocelsius(double kelvin) {
+		double celsius = kelvin - 273.15;
+		return celsius;
+	}
+
+	public static double fluidpressure(fluids_reee fluid, double deep) {
+		double fluidpressure = fluid.density * 9.82 * deep;
+		return fluidpressure;
+	}
+	// big
+}
