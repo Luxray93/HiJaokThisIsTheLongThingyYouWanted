e46324cb9d19d7d8a726879994fa4ead433e688d
Luxray93 <john117654@gmail.com>
2018-11-07 14:17:05 +0100

    reeeeeeeee 6 metoder måste commita 15 gånger så

--- src/enums_reeeeeeeeeeeeeeee/BigboieFysikgrej.java
+++ src/enums_reeeeeeeeeeeeeeee/BigboieFysikgrej.java
@@ -2,7 +2,9 @@ package enums_reeeeeeeeeeeeeeee;
 
 public class BigboieFysikgrej {
 	public static void main(String[] args) {
+		
 	}
+	
 
 	public static double fahrenheittocelsius(double fahrenheit) {
 
@@ -19,6 +21,18 @@ public class BigboieFysikgrej {
 	public static double fluidpressure(fluids_reee fluid, double deep) {
 		double fluidpressure = fluid.density * 9.82 * deep;
 		return fluidpressure;
+	}
+	public static double pressureunderwater(double deep) {
+		double pressureunderwater = 997*9.82*deep;
+		return pressureunderwater;
+	}
+	public static double kineticenergy(double mass, double velocity) {
+		double kineticenergy =  mass*velocity;
+		return kineticenergy;
+	}
+	public static double potentialenergy(double mass, double height) {
+		double potentialenergy = mass*height*9.82;
+		return potentialenergy;
 	}
 	// big
 }
