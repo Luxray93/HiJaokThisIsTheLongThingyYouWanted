418fc9f883e2844b09412dc593e58505e1f8b887
Luxray93 <john117654@gmail.com>
2019-02-01 14:57:05 +0100

    Update encoded.java

--- src/enums_reeeeeeeeeeeeeeee/encoded.java
+++ src/enums_reeeeeeeeeeeeeeee/encoded.java
@@ -8,9 +8,21 @@ public class encoded{
         Scanner Blyat = new Scanner(System.in);
         int testcases = Blyat.nextInt();
         Blyat.nextLine();
+        
+        for (int zax = 0; zax < testcases; zax++){
+            String str = Blyat.nextLine();
+            int len = (int)Math.sqrt(str.length());
+        
+            for (int j = 0; j < len; j++)
+            	for (int i = len - 1; i < str.length(); i += len)
+            		System.out.print(str.charAt(i - j));
+            	System.out.println();
+        }
 
+    Blyat.close();
+
+    }
         
 
     
-    }
 }
